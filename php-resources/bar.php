fa2692debb464bd89da4947a83fc777e9adc63ea
Edoardo <edoardo.guido.93@gmail.com>
2018-08-20 12:43:23 +0200

    Current state upload

---
+++ php-resources/bar.php
@@ -0,0 +1,5 @@
+<a href="index.php">Edoardo Guido</a> 
+<span>—</span> 
+<a href="index.php">About</a> 
+<span>—</span> 
+<a href="index.php">Contact</a>
---
+++ php-resources/head.php
@@ -0,0 +1,25 @@
+<title>Edoardo Guido</title>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta name="description" content="Communication designer with a passion for animation, based in Milan, Italy."/>
+<meta name="keywords" content="design, graphics, designer, animation, communication, 3D, 2D, motion"/>
+<meta name="author" content="Edoardo Guido"/>
+<meta name="copyright" content="Edoardo Guido"/>
+
+<link rel="stylesheet" href="style.css">
+
+<link rel="shortcut icon" type="image/ico" href="favicon.ico"/>
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<link rel="apple-touch-icon" href="favicon.ico">
+<link rel="mask-icon" href="favicon.svg" color="#6010f2">
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-105694460-1', 'auto');
+  ga('send', 'pageview');
+</script>
