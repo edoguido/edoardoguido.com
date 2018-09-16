ccf2173e2dea613a68f56ee086ce8480aad811a5
Edoardo <edoardo.guido.93@gmail.com>
2018-09-16 11:33:07 +0200

    Function cleaning, permanent animation

--- index.php
+++ index.php
@@ -15,89 +15,84 @@
                         </div>
                     </div>
                     <div class="section">
-                        <div class="right">
-                            <h1>Milan&#45;based communication designer<br>who loves to&nbsp;run, to&nbsp;cook and&nbsp;to&nbsp;geek.</h1>
+                        <h1>Milan&#45;based communication designer<br>who loves to&nbsp;run, to&nbsp;cook and&nbsp;to&nbsp;geek.</h1>
+                    </div>
+                    <div class="section">
+                        <div class="spoti">
+                        <script>
+                            var key     = 'f16b0724839e781f43ce4ad721297f13';
+                            var user    = 'drivinward';
+                            var limit   = 1;
+                            var what    = 'topartists'
+                            var url     = 'http://ws.audioscrobbler.com/2.0/?method=user.get' + what + '&user=' + user + '&api_key=' + key + '&limit=' + limit + '&period=1month&format=json';
+                            
+                            function startBars() {
+                                var bars = document.getElementsByClassName('bar');
+                                // console.log(bars.length);
+                                for (i = 0; i < bars.length; i++) {
+                                    bars[i].classList.add('animate');
+                                }
+                            }
+
+                            function stopBars() {
+                                var bars = document.getElementsByClassName('bar');
+                                // console.log(bars.length);
+                                for (i = 0; i < bars.length; i++) {
+                                    var childElement = bars[i];
+                                    // console.log(bars[i]);
+                                    bars[i].addEventListener("animationiteration webkitAnimationIteration", doStop(), false);
+                                }
+                                function doStop() {
+                                    bars[i].classList.remove('animate');
+                                }
+                            }
+
+                            function showMusicStats(item) {
+                                console.log(item.name);
+                                item.name = item.name.replace(/ /g,"&nbsp;");
+                                return `<h1>Currently listening to 
+                                    <a href="${item.url}" target="_blank" onmouseover="" onmouseout="">${item.name}</a>
+                                    <div id="bars">
+                                        <div class="bar animate"></div>
+                                        <div class="bar animate"></div>
+                                        <div class="bar animate"></div>
+                                    </div>
+                                </h1>`;
+                            };
+
+                            xmlhttp = new XMLHttpRequest();
+                            xmlhttp.open('GET', url, true);
+
+                            xmlhttp.onload = function() {
+                                if (xmlhttp.status >= 200 && xmlhttp.status < 400){
+                                    // Success!
+                                    // Array contenente tutti i dati ricvuti
+                                    var data = JSON.parse(xmlhttp.responseText);
+                                    // Elemento della pagina in cui posizionare i dati ricevuti
+                                    var el = document.getElementsByClassName("spoti");
+
+                                    // data.map() per inserire nell'elemento el[0] i risultati della chiamata AJAX
+                                    el[0].innerHTML = `${data.topartists.artist.map(showMusicStats).join('')}`;
+                                    // console.log(data.topartists);
+                                    } else {
+                                        // We reached our target server, but it returned an error
+                                    }
+                                };
+                            xmlhttp.onerror = function() {
+                                // There was a connection error of some sort
+                            };
+                                        
+                            xmlhttp.send();
+                        </script>
+
                         </div>
                     </div>
                     <div class="section">
-                        <div class="right">
-                            <div class="spoti">
-                            <script>
-                                var key     = 'f16b0724839e781f43ce4ad721297f13';
-                                var user    = 'drivinward';
-                                var limit   = 1;
-                                var what    = 'topartists'
-                                var url     = 'http://ws.audioscrobbler.com/2.0/?method=user.get' + what + '&user=' + user + '&api_key=' + key + '&limit=' + limit + '&period=1month&format=json';
-                                
-                                function startBars() {
-                                    var bars = document.getElementsByClassName('bar');
-                                    // console.log(bars.length);
-                                    for (i = 0; i < bars.length; i++) {
-                                        bars[i].classList.add('animate');
-                                    }
-                                }
-
-                                function stopBars() {
-                                    var bars = document.getElementsByClassName('bar');
-                                    // console.log(bars.length);
-                                    for (i = 0; i < bars.length; i++) {
-                                        var childElement = bars[i];
-                                        // console.log(bars[i]);
-                                        bars[i].addEventListener("animationiteration webkitAnimationIteration", doStop(), false);
-                                    }
-                                    function doStop() {
-                                        bars[i].classList.remove('animate');
-                                    }
-                                }
-
-                                function showItems(item) {
-                                    console.log(item.name);
-                                    return `<h1>Currently listening to 
-                                        <a href="${item.url}" target="_blank" onmouseover="startBars()" onmouseout="stopBars()">${item.name}</a> 
-                                        <div id="bars">
-                                            <div class="bar"></div>
-                                            <div class="bar"></div>
-                                            <div class="bar"></div>
-                                        </div>
-                                    </h1>`;
-                                };
-
-                                xmlhttp = new XMLHttpRequest();
-                                xmlhttp.open('GET', url, true);
-
-                                xmlhttp.onload = function() {
-                                    if (xmlhttp.status >= 200 && xmlhttp.status < 400){
-                                        // Success!
-                                        // Array contenente tutti i dati ricvuti
-                                        var data = JSON.parse(xmlhttp.responseText);
-                                        // Elemento della pagina in cui posizionare i dati ricevuti
-                                        var el = document.getElementsByClassName("spoti");
-
-                                        // data.map() per inserire nell'elemento el[0] i risultati della chiamata AJAX
-                                        el[0].innerHTML = `${data.topartists.artist.map(showItems).join('')}`;
-                                        // console.log(data.topartists);
-                                        } else {
-                                            // We reached our target server, but it returned an error
-                                        }
-                                    };
-                                xmlhttp.onerror = function() {
-                                    // There was a connection error of some sort
-                                };
-                                            
-                                xmlhttp.send();
-                            </script>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="section">
-                        <div class="right">
-                            <li class="links">
-                                <ul><a href="#" class="social link">Instagram</a></ul>
-                                <ul><a href="#" class="social link">LinkedIn</a></ul>
-                                <ul><a href="#" class="social link">E-Mail</a></ul>
-                            </li>
-                        </div>
+                        <li class="links">
+                            <ul><a href="#" class="social link">Instagram</a></ul>
+                            <ul><a href="#" class="social link">LinkedIn</a></ul>
+                            <ul><a href="#" class="social link">E-Mail</a></ul>
+                        </li>
                     </div>
                 </div>
 
