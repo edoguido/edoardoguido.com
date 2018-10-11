ebdbee96b28a7e2d6d56a246ec30a8ec659f5b7a
Edoardo <edoardo.guido.93@gmail.com>
2018-10-12 00:01:27 +0200

    Re-formatted and API request update

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <html>
 
-    <head>
-        <?php include'head.html'?>
-    </head>
+<head>
+    <?php include'head.html'?>
+</head>
 
-    <body onTouchStart="">
+<body onTouchStart="">
 
-        <div class="wrapper" id="main">
+    <div class="wrapper" id="main">
 
-            <!-- <input type="range" min="0" max="16" value="8" class="slider" id="myRange" oninput="inputChange()" style="position: absolute; display: block;"> -->
-            <div id="hero">
-                <div id="title" class="section">
-                    <div class="right">
-                        <h1>Edoardo Guido</h1>
-                    </div>
+        <!-- <input type="range" min="0" max="16" value="8" class="slider" id="myRange" oninput="inputChange()" style="position: absolute; display: block;"> -->
+        <div id="hero">
+            <div id="title" class="section">
+                <div class="right">
+                    <h1>Edoardo Guido</h1>
                 </div>
-                <div class="section">
-                    <h1>Milan&#45;based communication designer<br>who loves to&nbsp;run, to&nbsp;cook and&nbsp;to&nbsp;geek.</h1>
-                    <!-- <script>
+            </div>
+            <div class="section">
+                <h1>Milan&#45;based communication designer<br>who loves to&nbsp;run, to&nbsp;cook
+                    and&nbsp;to&nbsp;geek.</h1>
+                <!-- <script>
                         function inputChange() {
                             var slider = document.getElementById('myRange');
                             var hero = document.getElementById('hero');
@@ -25,17 +26,55 @@
                             hero.style.setProperty('padding', slider.value + '%');
                         }
                     </script> -->
-                </div>
-                <div class="section">
-                    <div class="spoti">
+            </div>
+            <div class="section">
+                <div class="spoti">
                     <script>
-                        var key     = '196f132fcffdbc0d1f4b75acaf1ca4bd';
+                        var key     = 'f979f1543fc51c076912222b9c1644fe';
                         var user    = 'drivinward';
                         var limit   = 1;
                         var what    = 'topartists';
                         var url     = 'http://ws.audioscrobbler.com/2.0/?method=user.get' + what + '&user=' + user + '&api_key=' + key + '&limit=' + limit + '&period=1month&format=json';
-                        
-                        function startBars() {
+                        var complete= 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=drivinward&api_key=f979f1543fc51c076912222b9c1644fe&limit=1&period=1month&format=json'
+
+                        function showMusicStats(item) {
+                            console.log(item.name);
+                            item.name = item.name.replace(/ /g, "&nbsp;");
+                            return `<h1>Currently listening to 
+                                <a href="${item.url}" target="_blank" onmouseover="" onmouseout="">${item.name} (${item.playcount} times)</a>
+                                <div id="bars">
+                                    <div class="bar animate"></div>
+                                    <div class="bar animate"></div>
+                                    <div class="bar animate"></div>
+                                </div>
+                            </h1>`;
+                        };
+
+                        xmlhttp = new XMLHttpRequest();
+                        xmlhttp.open('GET', url, true);
+
+                        xmlhttp.onload = function () {
+                            if (xmlhttp.status >= 200 && xmlhttp.status < 400) {
+                                // Success!
+                                // Array contenente tutti i dati ricevuti
+                                var data = JSON.parse(xmlhttp.responseText);
+                                // Elemento della pagina in cui posizionare i dati ricevuti
+                                var el = document.getElementsByClassName("spoti");
+
+                                // data.map() per inserire nell'elemento el[0] i risultati della chiamata AJAX
+                                el[0].innerHTML = `${data.topartists.artist.map(showMusicStats).join('')}`;
+                                // console.log(data.topartists);
+                            } else {
+                                // We reached our target server, but it returned an error
+                            }
+                        };
+                        xmlhttp.onerror = function () {
+                            // There was a connection error of some sort
+                        };
+
+                        xmlhttp.send();
+
+                                                function startBars() {
                             var bars = document.getElementsByClassName('bar');
                             // console.log(bars.length);
                             for (i = 0; i < bars.length; i++) {
@@ -56,101 +95,65 @@
                             }
                         }
 
-                        function showMusicStats(item) {
-                            console.log(item.name);
-                            item.name = item.name.replace(/ /g,"&nbsp;");
-                            return `<h1>Currently listening to 
-                                <a href="${item.url}" target="_blank" onmouseover="" onmouseout="">${item.name} (${item.playcount} times)</a>
-                                <div id="bars">
-                                    <div class="bar animate"></div>
-                                    <div class="bar animate"></div>
-                                    <div class="bar animate"></div>
-                                </div>
-                            </h1>`;
-                        };
-
-                        xmlhttp = new XMLHttpRequest();
-                        xmlhttp.open('GET', url, true);
-
-                        xmlhttp.onload = function() {
-                            if (xmlhttp.status >= 200 && xmlhttp.status < 400){
-                                // Success!
-                                // Array contenente tutti i dati ricevuti
-                                var data = JSON.parse(xmlhttp.responseText);
-                                // Elemento della pagina in cui posizionare i dati ricevuti
-                                var el = document.getElementsByClassName("spoti");
-
-                                // data.map() per inserire nell'elemento el[0] i risultati della chiamata AJAX
-                                el[0].innerHTML = `${data.topartists.artist.map(showMusicStats).join('')}`;
-                                // console.log(data.topartists);
-                                } else {
-                                    // We reached our target server, but it returned an error
-                                }
-                            };
-                        xmlhttp.onerror = function() {
-                            // There was a connection error of some sort
-                        };
-                                    
-                        xmlhttp.send();
                     </script>
 
-                    </div>
-                </div>
-                <div class="section">
-                    <li class="links">
-                        <ul><a href="https://www.instagram.com/edoguido_/" target="blank" class="social link">Instagram</a></ul>
-                        <ul><a href="https://www.linkedin.com/in/edoardoguido/" target="blank" class="social link">LinkedIn</a></ul>
-                        <ul><a href="mailto:contact@edoardoguido.com" class="social link">E-Mail</a></ul>
-                    </li>
                 </div>
             </div>
+            <div class="section">
+                <li class="links">
+                    <ul><a href="https://www.instagram.com/edoguido_/" target="blank" class="social link">Instagram</a></ul>
+                    <ul><a href="https://www.linkedin.com/in/edoardoguido/" target="blank" class="social link">LinkedIn</a></ul>
+                    <ul><a href="mailto:contact@edoardoguido.com" class="social link">E-Mail</a></ul>
+                </li>
+            </div>
+        </div>
 
-            <div class="content">
+        <div class="content">
 
-                <script type="text/javascript">
+            <script type="text/javascript">
 
-                    function prjTemplate(project) {
-                        // console.log(project));
-                        return `
+                function prjTemplate(project) {
+                    // console.log(project));
+                    return `
                             <div class="project ${project.id == 0 ? 'big' : ''} ${project.dark == true ? 'dark' : ''} json" data-title="${project.title}">
                                 <a href="${project.link}" class="project-link">
                                     <img class='lazy' src="res/${project.images['cover']}" data-src="res/${project.images['cover']}">
                                 </a>
                             </div>
                         `;
+                }
+
+                request = new XMLHttpRequest();
+                request.open('GET', 'projects.json', true);
+
+                request.onload = function () {
+                    if (request.status >= 200 && request.status < 400) {
+                        // Success!
+                        // Array contenente tutti i dati ricvuti
+                        var data = JSON.parse(request.responseText);
+                        // Elemento della pagina in cui posizionare i dati ricevuti
+                        var el = document.getElementsByClassName("content");
+
+                        // data.map() per inserire nell'elemento el[0] tutti i progetti del file .json
+                        el[0].innerHTML = `${data.map(prjTemplate).join('')}`;
+                    } else {
+                        // We reached our target server, but it returned an error
                     }
+                };
+                request.onerror = function () {
+                    // There was a connection error of some sort
+                };
 
-                    request = new XMLHttpRequest();
-                    request.open('GET', 'projects.json', true);
+                request.send();
 
-                    request.onload = function() {
-                        if (request.status >= 200 && request.status < 400){
-                            // Success!
-                            // Array contenente tutti i dati ricvuti
-                            var data = JSON.parse(request.responseText);
-                            // Elemento della pagina in cui posizionare i dati ricevuti
-                            var el = document.getElementsByClassName("content");
-                            
-                            // data.map() per inserire nell'elemento el[0] tutti i progetti del file .json
-                            el[0].innerHTML = `${data.map(prjTemplate).join('')}`;
-                            } else {
-                                // We reached our target server, but it returned an error
-                            }
-                    };
-                    request.onerror = function() {
-                        // There was a connection error of some sort
-                    };
-                    
-                    request.send();
-
-                </script>
-
-            </div>
+            </script>
 
         </div>
 
-    </body>
-    
-    <?php include 'feet.html'?>
+    </div>
+
+</body>
+
+<?php include 'feet.html'?>
 
 </html>
