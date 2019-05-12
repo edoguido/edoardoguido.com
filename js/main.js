1a6af40b40083ce52ac8978fe3a23082d510c8bd
Edoardo Guido <edoardo.guido.93@gmail.com>
2019-05-12 21:48:47 +0900

    Added content and project layout revamp

--- js/main.js
+++ js/main.js
@@ -15,9 +15,13 @@ document.addEventListener("DOMContentLoaded", function() {
 function prjList(project) {
     // console.log(project);
     return `
-        <a href="project.html?id=${project.id}" class="prj-href">
+        <a href="${project.href ? project.href : 'project.html?id=' + project.id }" target="${project.href ? '_blank' : ''}" class="prj-href">
             <div class="prj">
-                <div class="prj-cover" style="background-image: url('${project.cover}'); background-repeat: no-repeat; background-position: 50% 50%; background-size: cover; ${project.style ? project.style : ''}"></div>
+                <div class="prj-cover" background-repeat: no-repeat; background-position: 50% 50%; background-size: cover; ${project.style ? project.style : ''}">
+                    <video loop muted autoplay playsinline>
+                        <source src="${project.cover}"/>
+                    </video>
+                </div>
                 <div class="prj-data">
                     <h2>${project.title}</h2>
                     <h2>${project.category}</h2>
@@ -40,7 +44,7 @@ function initListeners() {
         }, false)
         
         prj.addEventListener("mouseout", function() {
-
+            
             this.parentElement.classList.remove("focus");
             
         }, false)
--- js/project.js
+++ js/project.js
@@ -7,19 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fetch(file).then((response) => {
         return response.json();
+
     }).then((entries) => {
 
-        console.log(entries);
-
-        const hero = document.getElementById("hero");
-
-        hero.style.backgroundImage = `url(${path}/_hero.${entries[0].type})`;
-        hero.style.backgroundColor = `${entries[0].bg}`;
-
+        // console.log(entries);
+        document.getElementById("info").innerHTML = entries[0].map(injectContent).join("");
         document.getElementById("showcase").innerHTML = entries[1].map(injectContent).join("");
 
     }).catch((err) => console.error(err))
-    
+
 })
 
 function injectContent(content) {
@@ -31,20 +27,58 @@ function injectContent(content) {
             return `
                 <div class="prj-p">
                     <div class="p-title">
-                        <p>${content.title}</p>
+                        <p>${content.title ? content.title : ''}</p>
                     </div>
                     <div class="p-desc">
                         <p>${content.content}</p>
                     </div>
                 </div>
             `;
+        case 'h1':
+            return `
+                <div class="prj-h1">
+                    <h1>${content.content}</h1>
+                </div>
+            `;
         case 'img':
             return `
-                <div class="prj-img" style="
-                    background-image: url(${path}/${content.path});
-                    background-size: ${content.bgsize ? content.bgsize : "cover"};
+                <img class="prj-img" src="${path}/${content.path}" 
+                    style="width: 100%; height: auto; background-size: ${content.bgsize ? content.bgsize : "cover"}; 
                     ${content.style ? content.style : ""}">
+                </img>
+            `;
+        case 'img-l':
+            return `
+                <a href="${content.href}" target="_blank">
+                    <img class="prj-img" src="${path}/${content.path}" 
+                        style="width: 100%; height: auto; ${content.style ? content.style : ""}">
+                    </img>
+                </a>
+            `;
+        case 'img-tight':
+            return `
+                <div class="prj-img-tight-container">
+                    <img class="prj-img-tight" src="${path}/${content.path1}" ${content.style ? content.style : ""}">
+                    </img>
+                    <img class="prj-img-tight" src="${path}/${content.path2}" ${content.style ? content.style : ""}">
+                    </img>
+                    <img class="prj-img-tight" src="${path}/${content.path3}" ${content.style ? content.style : ""}">
+                    </img>
                 </div>
+            `;
+        case 'video':
+            return `
+                <div class="prj-vid" style="${content.style ? content.style : ''}">
+                    <video loop muted playsinline autoplay>
+                        <source src="${path}/${content.path}"/>
+                    </video>
+                </div>
+            `;
+        case 'embed':
+            return `
+            <div class="embed-container">
+                ${content.href}
+            </div>
             `;
         default:
             console.log('tag not recognized');
