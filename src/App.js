348ded3cc2d4e8755668f345a584c69e9b89c175
edoguido <edoardo.guido.93@gmail.com>
2022-08-14 11:49:30 +0200

    feat: add Clarity analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'mobx-react'
+import { Helmet } from 'react-helmet'
 // import 'mobx-react-lite/batchingForReactDom'
 
 // Components
@@ -15,6 +16,20 @@ function App() {
   return (
     <Provider state={state}>
       <Router>
+        <Helmet>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "d3xhs4evhv");
+              `,
+            }}
+          />
+        </Helmet>
         <Nav />
         <Routes />
       </Router>
